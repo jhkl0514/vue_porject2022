17409feae60e5474f021dfd8ed203f6278934519
jhkl0514 <jhkl0514@naver.com>
2022-10-21 18:21:20 +0900

    1021

--- src/components/Main05Graph.vue
+++ src/components/Main05Graph.vue
@@ -1,57 +1,65 @@
 <template>
   <section class="wrap Main05Graph" id="graphGradient">
-    <div class="container">
-      <div class="graph-img">
-        <div><img src="../assets/main-number-1.jpg" alt="" /></div>
-        
+    <div class="container d-flex">
+      <div class="row col">
+
+      <div class="col col-md-4 d-flex align-items-center">
+        <div class="graph-img ">
+          <img src="../assets/main-number-1.jpg" alt="" />
+        </div>
       </div>
-      <ul class="graph-li">
-        <li>
-          <div class="graph-li-top">
-            <div class="graph-li-bar"><span></span></div>
-            <p class="num">+<span class="numCountCon1">0</span></p>
-          </div>
-          <div class="box">
-            <strong>지역 네트워크</strong>
-            <p>전국 17개 시도 200개이상의 지역에서 교육하고 있습니다.</p>
-          </div>
-        </li>
-        <li>
-          <div class="graph-li-top">
-            <div class="graph-li-bar"><span></span></div>
-            <p>+<span class="numCountCon2">0</span></p>
-          </div>
-          <div class="box">
-            <strong>학교 및 기관</strong>
-            <p>
-              전국 1,000여개 학교 및 기관, 단체와 협업하여 교육을 진행합니다.
-            </p>
-          </div>
-        </li>
-        <li>
-          <div class="graph-li-top">
-            <div class="graph-li-bar"><span></span></div>
-            <p>+<span class="numCountCon3">0</span></p>
-          </div>
-          <div class="box">
-            <strong>교육 진행자</strong>
-            <p>
-              3,000여명의 전문 지식을 갖춘 봉사자와 교사, 강사가 함께 교육을
-              진행하고 있습니다.
-            </p>
-          </div>
-        </li>
-        <li>
-          <div class="graph-li-top">
-            <div class="graph-li-bar"><span></span></div>
-            <p>+<span class="numCountCon4">0</span></p>
-          </div>
-          <div class="box">
-            <strong>교육 수혜자</strong>
-            <p>전국 90,000여명의 청소년들이 교육 프로그램에 참여합니다.</p>
-          </div>
-        </li>
-      </ul>
+
+      <div class="col col-md-8 mt-5 mb-4">
+        <ul class="graph-li">
+          <li>
+            <div class="graph-li-top">
+              <div class="graph-li-bar"><span></span></div>
+              <p class="num">+200</p>
+            </div>
+            <div class="box">
+              <strong>지역 네트워크</strong>
+              <p>전국 17개 시도 200개이상의 지역에서 교육하고 있습니다.</p>
+            </div>
+          </li>
+          <li>
+            <div class="graph-li-top">
+              <div class="graph-li-bar"><span></span></div>
+              <p class="num">+1,000</p>
+            </div>
+            <div class="box">
+              <strong>학교 및 기관</strong>
+              <p>
+                전국 1,000여개 학교 및 기관, 단체와 협업하여 교육을 진행합니다.
+              </p>
+            </div>
+          </li>
+          <li>
+            <div class="graph-li-top">
+              <div class="graph-li-bar"><span></span></div>
+              <p class="num">+3,000</p>
+            </div>
+            <div class="box">
+              <strong>교육 진행자</strong>
+              <p>
+                3,000여명의 전문 지식을 갖춘 봉사자와 교사, 강사가 함께 교육을
+                진행하고 있습니다.
+              </p>
+            </div>
+          </li>
+          <li>
+            <div class="graph-li-top">
+              <div class="graph-li-bar"><span></span></div>
+              <p class="num">+90,000</p>
+            </div>
+            <div class="box">
+              <strong>교육 수혜자</strong>
+              <p>전국 90,000여명의 청소년들이 교육 프로그램에 참여합니다.</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+
+      </div>
     </div>
   </section>
 </template>
@@ -62,13 +70,70 @@ export default {};
 
 <style lang="scss">
 .Main05Graph{
-  height: 470px;
-  background: #0B919C;
+  _height: 470px;
+  background: linear-gradient(89.94deg, #244B69 5.99%, #196673 100.79%);
   margin-bottom: 200px;
-  img{
+  .graph-img{   
+    img{      
     border-radius: 30px;
     width: 400px;   
     background: pink;
+    }
   }
+  .graph-li{
+    padding: 20px 0;
+    li{
+      margin-bottom: 10px;
+    }
+    .graph-li-top{
+      display: flex;
+      _justify-content:flex-start;
+      align-items: center;
+      .num{
+        font-size:30px;
+        color: white;
+        font-weight: 800;
+        line-height: 10px;
+        font-family: "NanumSquareB";
+      }
+      .graph-li-bar{
+      display: flex;
+      align-items: center;
+      height: 10px;
+      background: rgba(255, 255, 255, .5);
+      border-radius: 5px;
+      position: relative;
+      margin-right: 15px;
+      width: 75%;
+      _display: inline-block;
+      span{
+      display: block;
+      height: 100%;
+      width: 70%;
+      position: absolute;
+      top: 0;
+      left: 0;
+      _background: #fff;
+      border-radius: 5px;
+      background:#00a0af;
+      }
+    }
+    }
+  .box{
+    display: flex;
+    strong{
+      font-weight: 7oo;
+      color: #fff;
+      font-size:16px;
+      margin-right: 10px;
+      flex-shrink:0;
+    }
+    p{
+      color: rgba(255, 255, 255, .5);
+      font-size:14px;
+    }
+  }  
+  }
+  
 }
 </style>
--- src/components/Main08Movie.vue
+++ src/components/Main08Movie.vue
@@ -1,6 +1,26 @@
 <template>
-  <div>08무비배너</div>
+  <div class="rwap Main08Movie">
+      <div class="col">
+        <div class="Main08_video">
+          <video class="mainvdo" muted loop autoplay playsinline preload="metadata" poster="" id="mainVdo">
+                <source src="../assets/main-home-01-2.mp4" type="video/mp4">
+          </video>
+        </div>
+        <div class="txtWrap">
+            <div class="video-title">
+                    <ul>
+                        <li>JA KOREA가 함께합니다.</li>
+                    </ul>
+                </div>
+                <div class="video-subtitle">
+                        <p class="">우리 청소년들이 미래의 인재로 성장하도록</p>
+                </div>
+          </div>
+      </div>
+  </div>
 </template>
+
+
 
 <script>
 export default {
@@ -8,6 +28,21 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 
+.Man08Movie{
+  .Main08_video{
+    width: 100%;
+    padding-top: 60%;
+    position: relative;
+    overflow: hidden;
+  }
+  .mainvdo{
+    position: absolute;
+    top:0;
+    left: 0;
+    object-fit: cover;
+  }
+
+}
 </style>
